805f6e73e9292a53855d00017de30ba1851b0672
Rick3Mrtz <Rick.mrtz3@gmail.com>
2023-02-09 00:58:37 -0600

    created Server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+const htmlRoutes = require('./routes/html');
+const apiRoutes = require('./routes/api');
+
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+//routes  
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
